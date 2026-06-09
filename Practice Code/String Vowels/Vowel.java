06fefbe9972581200fc3bef692bc77ef6cdb5995
Bhavya <bhavyakhatri1235@gmail.com>
2026-06-09 11:02:28 +0530

    String Codes

--- Practice Code/String Vowels/Vowel.java
+++ Practice Code/String Vowels/Vowel.java
@@ -3,16 +3,16 @@ import java.util.Scanner;
 public class Vowel{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
-        System.out.print("Enter a string: ");
-        String str = sc.next();
+        System.out.print("Enter a Word: ");
+        String str = sc.next().toLowerCase();
         int count = 0;
         for(int i=0; i<str.length(); i++){
             char c=str.charAt(i);
-            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' || c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U'){
+            if(c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u' ){
                 count++;
             }
         }
-        System.out.println("Number of vowels in the string: " + count);
+        System.out.println("Number of vowels in the word: " + count);
         sc.close();
     }
 }
